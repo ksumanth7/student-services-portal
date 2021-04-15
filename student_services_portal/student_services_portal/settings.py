d0398cbd1aee565f31c32a4427c08040284a6719
ksumanth7 <kasula0101@gmail.com>
2021-04-15 15:22:52 +0530

    update

--- student_services_portal/student_services_portal/settings.py
+++ student_services_portal/student_services_portal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student_services_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'student_services_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_services_portal',
+        'USER': 'student_services_portal',
+        'PASSWORD': 'student_services_password',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
